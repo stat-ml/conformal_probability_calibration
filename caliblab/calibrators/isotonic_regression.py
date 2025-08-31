fix(calibrator): softmax

caliblab/calibrators/isotonic_regression.py
@@ -1,6 +1,7 @@
 from typing import Optional
 
 import numpy as np
+import torch
 from sklearn.isotonic import IsotonicRegression as SklearnIsotonicRegression
 
 from .base import CalibratorBase
@@ -22,11 +23,12 @@ class IsotonicRegression(CalibratorBase):
         probs: Optional[np.ndarray] = None,
         y_true: np.ndarray,
     ) -> "IsotonicRegression":
-        if probs is None and logits is None:
-            raise ValueError("IsotonicRegression requires probabilities (probs).")
-
         if probs is None:
-            probs = np.softmax(logits, axis=1)
+            if logits is None:
+                raise ValueError(
+                    "Either logits or probs must be provided to IsotonicRegression."
+                )
+            probs = torch.softmax(torch.from_numpy(logits), dim=1).numpy()
 
         n_classes = probs.shape[1]
         self.calibrators = [
@@ -49,11 +51,12 @@ class IsotonicRegression(CalibratorBase):
         probs: Optional[np.ndarray] = None,
     ) -> np.ndarray:
         self.check_fitted()
-        if probs is None and logits is None:
-            raise ValueError("IsotonicRegression requires probabilities or logits (probs).")
-
         if probs is None:
-            probs = np.softmax(logits, axis=1)
+            if logits is None:
+                raise ValueError(
+                    "Either logits or probs must be provided to IsotonicRegression."
+                )
+            probs = torch.softmax(torch.from_numpy(logits), dim=1).numpy()
 
         n_samples, n_classes = probs.shape
         calibrated_probs = np.zeros_like(probs)

caliblab/eval/evaluator.py
@@ -14,7 +14,6 @@ from ..datasets.base import BaseDataset
 from ..metrics import MetricBase
 from ..models import ModelBase
 from .constants import EvaluationReport
-from ..utils.computations import softmax
 
 
 class ModelEvaluator:
@@ -109,10 +108,10 @@ class ModelEvaluator:
             print(f"\nEvaluating calibrator: {calibrator_name}")
             logits = deepcopy(test_logits)
             if calibrator is not None:
-                calibrator.fit(all_logits=cal_logits, y_true=cal_labels)
-                final_probs = calibrator.predict_proba(probs=logits)
+                calibrator.fit(logits=cal_logits, y_true=cal_labels)
+                final_probs = calibrator.predict_proba(logits=logits)
             else:
-                final_probs = np.softmax(logits, dim=1)
+                final_probs = torch.softmax(torch.from_numpy(logits), dim=1).numpy()
 
             calibrated_metrics = {}
             for metric in self.metrics: