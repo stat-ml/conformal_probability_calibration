26f0c7281095d49695a376348716c5e19c6a72c9
Daniil Kazantsev <daniil.kazantsev@mbzuai.ac.ae>
2025-09-05 17:22:07 +0400

    Build(metrics): added roc_auc and pr_auc

--- caliblab/metrics/classification.py
+++ caliblab/metrics/classification.py
@@ -1,7 +1,8 @@
 import numpy as np
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, roc_auc_score, average_precision_score
 
 from .base import LabelBasedMetricBase
+from ..utils.computations import make_one_hot
 
 
 class Accuracy(LabelBasedMetricBase):
@@ -14,3 +15,26 @@ class Accuracy(LabelBasedMetricBase):
     def _compute(self, *, probs: np.ndarray, y_true: np.ndarray, **kwargs) -> float:
         y_pred = np.argmax(probs, axis=1)
         return accuracy_score(y_true, y_pred)
+
+
+class RocAuc(LabelBasedMetricBase):
+    """Computes the Area Under the Receiver Operating Characteristic Curve."""
+
+    @property
+    def name(self) -> str:
+        return "roc_auc"
+
+    def _compute(self, *, probs: np.ndarray, y_true: np.ndarray, **kwargs) -> float:
+        return roc_auc_score(y_true, probs, multi_class="ovr", average="macro")
+
+
+class PrAuc(LabelBasedMetricBase):
+    """Computes the Area Under the Precision-Recall Curve."""
+
+    @property
+    def name(self) -> str:
+        return "pr_auc"
+
+    def _compute(self, *, probs: np.ndarray, y_true: np.ndarray, **kwargs) -> float:
+        y_true_one_hot = make_one_hot(y_true, n_classes=probs.shape[1])
+        return average_precision_score(y_true_one_hot, probs, average="macro")
